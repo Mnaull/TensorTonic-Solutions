bb6e0b4d9a874a78b24c82654ebcc80e391912ef
Mnaull <152295481+Mnaull@users.noreply.github.com>
2026-03-10 11:36:05 +0000

    Update solution for jaccard-similarity

--- jaccard-similarity/jaccard-similarity.py
+++ jaccard-similarity/jaccard-similarity.py
@@ -4,12 +4,9 @@ def jaccard_similarity(set_a, set_b):
     """
     set_a=set(set_a)
     set_b=set(set_b)
-    inter=0
+    inter=set_a&set_b
+    union=set_a|set_b
     
-    for a in set_a:
-        if a in set_b:
-            inter+=1
-    union=len(set_a)+len(set_b)-inter
-    if union==0:
+    if len(union)==0:
         return 0
-    return inter/union
+    return len(inter)/len(union)
